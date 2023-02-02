7f8d2e4b25fb742fda7a8642a698b14f0c146223
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2023-02-02 15:02:32 +0900

    instagram urls 연결

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet) # 2개 URL을 만들어 줍니다.
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
