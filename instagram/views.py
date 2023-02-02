9b206c1e63fbc83bbd23f1821d2b3161e0072e9b
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2023-02-02 15:51:11 +0900

    JSON 응답뷰 만들기

--- instagram/views.py
+++ instagram/views.py
@@ -5,6 +5,22 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
 from .models import Post
 
+
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     # print 비추천, logger 추천(활용성)
+    #     """ POST:json(httpie)
+    #     requset.body:  b'{"message": "\\ub124\\ubc88\\uc9f8 \\ud3ec\\uc2a4\\ud305\\uc785\\ub2c8\\ub2e4"}'
+    #     requset.POST:  <QueryDict: {}>
+    #     """
+    #     """ form(httpie)
+    #     requset.body:  b'message=%EB%84%A4%EB%B2%88%EC%A7%B8+%ED%8F%AC%EC%8A%A4%ED%8C%85%EC%9E%85%EB%8B%88%EB%8B%A4'
+    #     requset.POST:  <QueryDict: {'message': ['네번째 포스팅입니다']}>
+    #
+    #     """
+    #     print("requset.body: ", request.body)
+    #     print("requset.POST: ", request.POST)
+    #     return super().dispatch(request, *args, **kwargs)
