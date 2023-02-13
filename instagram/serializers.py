0b2f09e686449d2f6a2b7d21d06e41c5bc4339ae
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2023-02-13 12:24:22 +0900

    Serializer를 통한 유효성 검사 및 저장

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -23,4 +23,5 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'is_public',
+            'ip',
         ]
--- instagram/views.py
+++ instagram/views.py
@@ -37,7 +37,13 @@ def public_post_list(request):
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    # authentication_classes = []
 
+    def perform_create(self, serializer):
+        # FIXME: 인증이 되어있다는 가정하에, author를 지정해보겠습니다.
+        author = self.request.user # User or AnonymousUser
+        ip = self.request.META['REMOTE_ADDR']
+        serializer.save(author=author, ip=ip)
 
     @action(detail=False, methods=['GET'])
     def public(self, request):
